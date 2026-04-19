73bc1229f534a03e50222980cfbbd613f1e96e22
elohsistemas <elohsistemas@gmail.com>
2026-04-19 19:40:58 -0300

    alteração de ip

--- isolvus.site/src/servidor/api.jsx
+++ isolvus.site/src/servidor/api.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 //const URL = "https://10.50.7.88:3010";
 //const URL = "http://localhost:3011";
 //const URL = "https://sgs.serrana.agr.br:58624";
-const URL = "http://192.168.0.10:3011";
+const URL = "http://192.168.0.4:3011";
 //const URL = "http://10.85.106.11:3011";
 
 const api = axios.create({
