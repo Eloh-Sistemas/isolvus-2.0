5ae8fa7727d01d956e875c2580950bb0cb58ddc2
elohsistemas <elohsistemas@gmail.com>
2026-04-19 19:33:26 -0300

    Caminho do instalclient

--- isolvus.api.site/src/config/database.js
+++ isolvus.api.site/src/config/database.js
@@ -4,7 +4,7 @@ import oracledb from 'oracledb';
 import { dbConfig } from './dbConfig.js'; // Configurações do banco de dados
 
 oracledb.initOracleClient({
-  libDir: '/opt/oracle/instantclient_21_13'
+  libDir: '/opt/oracle/instantclient_21_12'
 });
 
 export async function getConnection() {
